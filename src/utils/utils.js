c2cb5a4a53288816214c223b62fbbfd722f6c4b1
gogu <gogu9909@gmail.com>
2022-05-14 22:36:00 +0800

    chore: Use randomUUID polyfill.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,11 +2,18 @@ import dayjs from 'dayjs'
 
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
+crypto.randomUUID = function randomUUID() {
+  return (
+    [1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
+    c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+};
+
 export const getUserId = () => {
   const local = localStorage.getItem('voting/userid')
   if (local) return local
 
-  const id = window.crypto.randomUUID().slice(0, 8)
+  const id = crypto.randomUUID().slice(0, 8)
   localStorage.setItem('voting/userid', `0x${id}`)
   return id
 }
